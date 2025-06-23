a03003b59c89e6b67398a00256184ff8679314cd
Raegrand <raegrand@gmail.com>
2025-06-23 15:38:07 +0700

    Create Code_IoT_2025.ino

---
+++ Code_IoT_2025/Code_IoT_2025.ino
@@ -0,0 +1,14 @@
+/*  CODING IOT TRACKER SHUTTLE ITB SI BIRU 2025 
+    Developed by : Raegrand Archamadeus, Zaki Azzamy, Kenny Pramanik
+    Last Update  : 24/06/2025 
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
